3dc22ffe7d691913f8dea8657e93276a9bd59409
MeterSoft <metersoft@ubuntu.(none)>
2014-02-10 11:15:31 +0200

    popup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,17 @@ class UsersController < ApplicationController
 	
 	def new
 		@user = User.new
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
 
+	def edit
+		respond_to do |format|
+			format.html
+			format.js
+		end
 	end
 
 	def create
